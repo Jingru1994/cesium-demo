0269cfb18d8e202f6492778bf6476b08cd335cc2
jingru <dongjingru2012@163.com>
2021-07-06 16:27:15 +0800

    three的macroScene下沉动态效果

--- src/components/tilesetPhotogrammetry1.vue
+++ src/components/tilesetPhotogrammetry1.vue
@@ -38,7 +38,14 @@ export default {
             let tileset = new Cesium.Cesium3DTileset(options);
             viewer.scene.primitives.add(tileset);
             const that = this;
-            tileset.readyPromise.then(function (tileset) {
+            tileset.readyPromise.then(function (tileset) {               
+                let boundingSphere = tileset.boundingSphere;
+                let cartographic = Cesium.Cartographic.fromCartesian(boundingSphere.center);//获取到倾斜数据中心点的经纬度坐标（弧度）
+                console.log(cartographic);
+                let surface = Cesium.Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, 0.0);//倾斜数据中心点的笛卡尔坐标 
+                let offset =Cesium.Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, 13);//带高程的新笛卡尔坐标
+                let translation = Cesium.Cartesian3.subtract(offset, surface, new Cesium.Cartesian3());
+                tileset.modelMatrix = Cesium.Matrix4.fromTranslation(translation);
                 that.$emit("readyPromise", tileset);
             }).otherwise(function (error) {
                 console.log(error);
