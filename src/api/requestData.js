f11f99c9dda4187d6b36bb9e3dfe710a3f819306
jingru <dongjingru2012@163.com>
2022-01-07 18:48:01 +0800

    修复生产环境请求public中数据路径问题

--- src/api/requestData.js
+++ src/api/requestData.js
@@ -6,41 +6,46 @@
  * @Description: In User Settings Edit
  * @FilePath: \cesium-demo\src\api\requestGeojson.js
  */
-import axios from 'axios'
+import axios from "axios";
 
-export function  getPublicData(name) {
-    // console.log(process.env.BASE_URL);
-    // console.log( window.location.pathname);
-    let prefix = process.env.NODE_ENV === "production" ? window.location.pathname : "";
-    return axios.get(prefix + `${name}`).then((data)=>{
-        if(data.data){
-            return Promise.resolve(data.data)
-        }
-        return Promise.reject({
-            returnCode :-1
-        })
-    })
+export function getPublicData(name) {
+  // console.log(process.env.BASE_URL);
+  // console.log( window.location.pathname);
+  let prefix =
+    process.env.NODE_ENV === "production" ? window.location.pathname : "";
+  console.log(prefix);
+  return axios.get(`${name}`).then(data => {
+    if (data.data) {
+      return Promise.resolve(data.data);
+    }
+    return Promise.reject({
+      returnCode: -1
+    });
+  });
 }
 
-export function  toGeojson(json) {
-    let features = json;
-    let elements = [];
-    for(let i = 0; i<features.length; i++){
-        let out_feature = {};
-        out_feature["type"] = "Feature";
-        out_feature["properties"] = {};
-        for(let property in features[i]){
-            out_feature["properties"][property] = features[i][property];
-        }
-        out_feature["geometry"] = {}
-        out_feature["geometry"]["type"] = "Point"
-        out_feature["geometry"]["coordinates"] = [parseFloat(features[i]['j']), parseFloat(features[i]['w'])]
-        elements.push(out_feature)
+export function toGeojson(json) {
+  let features = json;
+  let elements = [];
+  for (let i = 0; i < features.length; i++) {
+    let out_feature = {};
+    out_feature["type"] = "Feature";
+    out_feature["properties"] = {};
+    for (let property in features[i]) {
+      out_feature["properties"][property] = features[i][property];
     }
-    let final_json = {
-        "type": "FeatureCollection",
-        "name": "tmp",
-        "features": elements
-    };
-    return final_json;
+    out_feature["geometry"] = {};
+    out_feature["geometry"]["type"] = "Point";
+    out_feature["geometry"]["coordinates"] = [
+      parseFloat(features[i]["j"]),
+      parseFloat(features[i]["w"])
+    ];
+    elements.push(out_feature);
+  }
+  let final_json = {
+    type: "FeatureCollection",
+    name: "tmp",
+    features: elements
+  };
+  return final_json;
 }
